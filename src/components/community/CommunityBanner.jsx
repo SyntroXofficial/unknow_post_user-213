d77a3aacde950f63f96591417c30c06a340fe943
SyntroXofficial <162024542+SyntroXofficial@users.noreply.github.com>
2025-01-29 10:15:31 +0200

    Update CommunityBanner.jsx

--- src/components/community/CommunityBanner.jsx
+++ src/components/community/CommunityBanner.jsx
@@ -2,15 +2,13 @@ import React from 'react';
 
 function CommunityBanner() {
   return (
-    <div className="h-[50vh] relative">
+    <div className="h-[35vh] relative">
       <img 
         src="https://mrwallpaper.com/images/hd/minimalist-neon-lights-4k-purple-40k82xyg4n3au9cn.jpg"
         alt="Community Banner"
         className="w-full h-full object-cover"
       />
-      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/50 to-[#030303]" />
-      <div className="absolute bottom-0 left-0 right-0 p-4">
-      </div>
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/50 to-black" />
     </div>
   );
 }
